694fc3fc81a45493099dc3443ae735b301e37990
herme <hermes7308@gmail.com>
2024-08-11 00:53:42 -0700

    Fix Player rotation issue and set FlatKit

--- Assets/Scripts/KillerRobot.cs
+++ Assets/Scripts/KillerRobot.cs
@@ -1,15 +1,26 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Animations.Rigging;
 
 public class KillerRobot : MonoBehaviour
 {
     [SerializeField] InputManager inputManager;
     [SerializeField] float speed;
 
+    RigBuilder rigBuilder;
+
+    void Awake()
+    {
+        rigBuilder = GetComponent<RigBuilder>();
+    }
+
     void Update()
     {
-        Vector3 newPosition = speed * Time.deltaTime * new Vector3(inputManager.inputVec.x, 0, inputManager.inputVec.y);
-        transform.Translate(newPosition);
+        Vector3 forwardPosition = speed * Time.deltaTime * new Vector3(0, 0, inputManager.inputVec.y);
+        transform.Translate(forwardPosition);
+
+        Vector3 sidePosition = speed * Time.deltaTime * new Vector3(0, inputManager.inputVec.x * 10, 0);
+        transform.Rotate(sidePosition);
     }
 }
